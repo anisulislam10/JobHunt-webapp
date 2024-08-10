dbab2fd37c2082ac7b60ede902d4717581988966
Anisul Islam <anis.inbox10@gmail.com>
2024-08-10 06:02:37 -0700

    added api end point into index.js

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
+
+import userRoute from "./../backend/routes/users.routes.js"
 dotenv.config({})
 const app=express();
 // app.get("/", (req,res)=>{
@@ -22,6 +24,11 @@ const corsOptions={
 
 app.use(cors(corsOptions));
 const PORT=process.env.PORT || 3000;
+
+//api s
+app.use("/api/v1/user", userRoute);
+
+
 app.listen(PORT,()=>{
     connectDB();
     console.log(`Server Running at Port: >> ${PORT}  `);
