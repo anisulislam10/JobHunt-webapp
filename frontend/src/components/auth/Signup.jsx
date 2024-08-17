b62efa5051708d167f58cb47fc0c4767fc566b05
Anisul Islam <anis.inbox10@gmail.com>
2024-08-17 02:26:47 -0700

    integerate signin and signup ui with backend endpoint apis using axios

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -5,14 +5,13 @@ import { Input } from '../ui/input'
 import { RadioGroup } from '../ui/radio-group'
 import { RadioGroupItem } from '../ui/radio-group'
 import { Button } from '../ui/button'
-
-
-
-
-
+import axios from "axios"
+// import { Toaster } from "@/components/ui/sonner"
+import { toast } from 'sonner'
 import { useState } from 'react';
-import { Link } from 'react-router-dom'
+import { Link, useNavigate  } from 'react-router-dom'
 import { LogIn } from 'lucide-react'
+import { USER_API_END_POINT } from '../../utils/api'
 
 
 function Signup() {
@@ -31,6 +30,7 @@ function Signup() {
     file: ""
 
   });
+  const navigate = useNavigate();
   const changeEventHandler = (e) => {
     setInput({ ...input, [e.target.name]: e.target.value });
   }
@@ -40,7 +40,33 @@ function Signup() {
   }
   const submitHandler = async (e) => {
     e.preventDefault();
-    console.log(input);
+    // console.log(input);
+    const formData = new FormData();
+    formData.append("fullname", input.fullname);
+    formData.append("email", input.email);
+    formData.append("phoneNumber", input.phoneNumber);
+    formData.append("password", input.password);
+    formData.append("role", input.role);
+    if (input.file) {
+      formData.append("file", input.file);
+    }
+
+    try {
+      const res = await axios.post(`${USER_API_END_POINT}/register`, formData, {
+        headers: {
+          'Content-Type': "multipart/form-data"
+        },
+        withCredentials: true
+      });
+      if (res.data.success) {
+        navigate('/login');
+        toast.success(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.message);
+
+    }
 
 
   }
@@ -121,7 +147,7 @@ function Signup() {
                   onChange={changeEventHandler}
                   className="cursor-pointer"
                 />
-                 <Label htmlFor="student">Student</Label> {/* Updated `htmlFor` to match `id` */}
+                <Label htmlFor="student">Student</Label> {/* Updated `htmlFor` to match `id` */}
 
                 {/* <Label htmlFor="r1">Student</Label> */}
               </div>
@@ -138,7 +164,7 @@ function Signup() {
                   onChange={changeEventHandler}
                   className="cursor-pointer"
                 />
-                  <Label htmlFor="recruiter">Recruiter</Label> {/* Updated `htmlFor` to match `id` */}
+                <Label htmlFor="recruiter">Recruiter</Label> {/* Updated `htmlFor` to match `id` */}
 
                 {/* <Label htmlFor="r2">Recruiter</Label> */}
               </div>
