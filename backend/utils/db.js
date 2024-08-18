f1c333ef147c3d77e0c70b54cf481fb974c4240b
Anisul Islam <anis.inbox10@gmail.com>
2024-08-18 01:31:06 -0700

    modified mongodb connection

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,13 +1,23 @@
 import mongoose from "mongoose";
-const connectDB =async()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("-------------------------------------");
-        console.log("***MongoDB Connected Successfully***"); 
-        console.log("-------------------------------------");
 
-    } catch (error) {
-        console.log(error);
-    }
-}
+let isConnected;
+
+const connectDB = async () => {
+  if (isConnected) {
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = true;
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
+};
+
 export default connectDB;
