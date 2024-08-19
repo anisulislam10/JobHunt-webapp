e10ccaef7017f36c8be768932d3cde7db6144d69
Anisul Islam <anis.inbox10@gmail.com>
2024-08-19 06:00:30 -0700

    added user into redux for save the the user

--- frontend/src/redux/authSlice.js
+++ frontend/src/redux/authSlice.js
@@ -3,13 +3,17 @@ import { createSlice } from "@reduxjs/toolkit";
 const authSlice = createSlice({
     name: "auth",
     initialState:{
-        loading: false
+        loading: false,
+        user:null
     },
     reducers:{
      //actions
      setLoading:(state, action)=>{
         state.loading=action.payload;
      }
+    },
+    setUser:(state,action)=>{
+        state.user=action.payload;
     }
 });
 export const {setLoading}=authSlice.actions;
